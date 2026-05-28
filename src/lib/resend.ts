1dbae564277ed6c3aff2600b366985d1b3a5814c
Dennis Marjanov <marjanov.den@gmail.com>
2026-05-28 21:41:10 +0100

    Fix Resend client initialization for Vercel builds

--- src/lib/resend.ts
+++ src/lib/resend.ts
@@ -1,6 +1,21 @@
 import { Resend } from 'resend'
 
-export const resend = new Resend(process.env.RESEND_API_KEY)
+let resendClient: Resend | null = null
+
+function getResendClient() {
+  if (!process.env.RESEND_API_KEY) {
+    throw new Error('RESEND_API_KEY is not configured')
+  }
+
+  resendClient ??= new Resend(process.env.RESEND_API_KEY)
+  return resendClient
+}
+
+export const resend = {
+  emails: {
+    send: (...args: Parameters<Resend['emails']['send']>) => getResendClient().emails.send(...args),
+  },
+}
 
 export const FROM_EMAIL = process.env.RESEND_FROM_EMAIL ?? 'hello@racketlogic.co.uk'
 export const ADMIN_EMAIL = process.env.RESEND_ADMIN_EMAIL ?? 'admin@racketlogic.co.uk'
